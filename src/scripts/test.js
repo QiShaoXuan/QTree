eb56b02e600ef73b31b29926d140728acd84c116
QiShao <474021214@qq.com>
2017-12-04 00:58:54 +0800

    添加了初始化时默认打开节点的设置

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,3 +1,24 @@
+let mockData = [
+  {id: "32", pid: "3", index: 1, name: "叶子节点 3-2", allArea: '4', rentArea: '3'},
+  {id: "1", pid: "0", name: "父节点 1", allArea: '1234', rentArea: '56789123'},
+  {id: "112", pid: "11", name: "叶子节点 1-1-2", allArea: '1234', rentArea: '56789123'},
+  {id: "22", pid: "2", index: 1, name: "叶子节点 2-2", allArea: '5', rentArea: '2'},
+  {id: "1120", pid: "112", name: "叶子节点 1-1-2-0", allArea: '1234', rentArea: '56789123'},
+  {id: "12", pid: "1", name: "叶子节点 1-2", allArea: '4', rentArea: '56789123'},
+  {id: "13", pid: "1", name: "叶子节点 1-3", allArea: '4', rentArea: '56789123'},
+  {id: "2", pid: "0", index: 0, name: "父节点 2", allArea: '15', rentArea: '6'},
+  {id: "21", pid: "2", index: 1, name: "叶子节点 2-1", allArea: '5', rentArea: '2'},
+  {id: "11", pid: "1", name: "叶子节点 1-1", allArea: '1234', rentArea: '56789123'},
+  {id: "110", pid: "11", name: "叶子节点 1-1-0", allArea: '1234', rentArea: '56789123'},
+  {id: "111", pid: "11", name: "叶子节点 1-1-1", allArea: '1234', rentArea: '56789123'},
+  {id: "1121", pid: "112", name: "叶子节点 1-1-2-1", allArea: '1234', rentArea: '56789123'},
+  {id: "23", pid: "2", index: 1, name: "叶子节点 2-3", allArea: '5', rentArea: '2'},
+  {id: "3", pid: "0", index: 0, name: "父节点 3", allArea: '12', rentArea: '9'},
+  {id: "31", pid: "3", index: 1, name: "叶子节点 3-1", allArea: '4', rentArea: '3'},
+  {id: "1122", pid: "112", name: "叶子节点 1-1-2-2", allArea: '1234', rentArea: '56789123'},
+  {id: "33", pid: "3", index: 1, name: "叶子节点 3-3", allArea: '4', rentArea: '3'},
+];
+
 let tree = new Qtree('#tree-container', mockData, {
   branchFormatter: `<div class="QTree-branch">
                       <span class="tree-content" name></span>
@@ -12,6 +33,7 @@ let tree = new Qtree('#tree-container', mockData, {
                       </div>
                     </div>`,
   renderData: ['name', 'allArea', 'rentArea'],
+  openBranch:1120
 
 })
 
@@ -39,4 +61,6 @@ $('#treeModal #tree-modal-sure').on('click', function () {
     tree.updateBranch(id,newData)
   }
   $('#treeModal').modal('hide')
-})
+});
+
+console.log(tree)
